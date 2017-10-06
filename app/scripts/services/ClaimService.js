d600fa0e101551021a69c35ba030ee78aeda8295
patricklx <patricklx@users.noreply.github.com>
2017-10-06 14:40:34 +0200

    remove query param all (#12)
    keep for user and claim endpoint (custom backend)

--- app/scripts/services/ClaimService.js
+++ app/scripts/services/ClaimService.js
@@ -4,5 +4,13 @@ angular.module('BlurAdmin.services').factory('claimService', function(
   BaseService, backendProtocol, backendHost, backendPath) {
 
   var backendUrl = backendProtocol + '://' + backendHost + backendPath + '/';
-  return new BaseService('claims', backendUrl);
+
+  var service = new BaseService('claims', backendUrl);
+  var oldFindAll = service.findAll;
+  service.findAll = function (queryParams) {
+    queryParams = queryParams || {};
+    queryParams.userId = queryParams.userId || 'all';
+    return oldFindAll.apply(this, queryParams);
+  };
+  return service;
 });
