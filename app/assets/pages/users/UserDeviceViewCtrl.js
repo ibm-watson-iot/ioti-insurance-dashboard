68601ff00dcd2a07c220750e2eb0cb08a3e9a536
Bilal Al-Saeedi <bilal.alsaeedi@de.ibm.com>
2017-10-13 17:12:55 +0200

    Show location on map in device view

--- app/assets/pages/users/UserDeviceViewCtrl.js
+++ app/assets/pages/users/UserDeviceViewCtrl.js
@@ -3,14 +3,30 @@
 
   angular.module('BlurAdmin.pages.users').controller('UserDeviceViewCtrl', UserDeviceViewCtrl);
 
-  function UserDeviceViewCtrl($stateParams, deviceService) {
+  function UserDeviceViewCtrl($stateParams, deviceService, gmapsHandler) {
     var vm = this;
     vm.userDevice = {};
 
     if ($stateParams.deviceId) {
       deviceService.find($stateParams.deviceId).success(function(device) {
-        vm.userDevice = device.devices;
+        vm.userDevice = device;
+        showInMap(device);
       });
     }
+
+    const showInMap = function(device) {
+      gmapsHandler.initGmaps();
+      if (device.location
+        && device.location.geometry
+        && device.location.geometry.coordinates) {
+        gmapsHandler.showInMap({
+          type: 'latLng',
+          latLng: {
+            lat: device.location.geometry.coordinates[0],
+            lng: device.location.geometry.coordinates[1]
+          }
+        });
+      }
+    };
   }
 })();
