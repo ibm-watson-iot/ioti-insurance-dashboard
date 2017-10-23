db527f401132b3c3b06cc2b5ca9be762611c1f62
Bilal Al-Saeedi <bilal_alsaidi@yahoo.com>
2017-10-23 10:28:14 +0200

    Show device hazard location (#18)
    * Show location in hazard view
    * Remove map from user view since it is on the device level
    * Show location on map in device view

--- app/assets/pages/users/userViewCtrl.js
+++ app/assets/pages/users/userViewCtrl.js
@@ -24,7 +24,6 @@ function UserViewCtrl($stateParams, userService, shieldService, shieldActivation
 
     userService.find($stateParams.userId).success(function(user) {
       vm.user = user;
-      initializeLocationMap(user.address.street + ", " + user.address.zipcode + " " + user.address.city + ", " + user.address.country);
       shieldActivationService.findAll({userId: $stateParams.userId}).success(function(data) {
         vm.userShields = vm.userShields.concat(data.items);
       });
@@ -44,50 +43,6 @@ function UserViewCtrl($stateParams, userService, shieldService, shieldActivation
         console.log('Failed to retrieve claims !');
       });
     });
-  }
-
-  function initializeLocationMap(address) {
-    var geocoder;
-    var map;
-
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-      zoom: 14,
-      center: latlng,
-      mapTypeControl: true,
-      mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-      navigationControl: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById("google-maps"), myOptions);
-    if (geocoder) {
-      geocoder.geocode({'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-            map.setCenter(results[0].geometry.location);
-            var infowindow = new google.maps.InfoWindow(
-              {
-                content: '<b>' + address + '</b>',
-                size: new google.maps.Size(150, 50)
-              });
-            var marker = new google.maps.Marker({
-              position: results[0].geometry.location,
-              map: map,
-              title: address
-            });
-            google.maps.event.addListener(marker, 'click', function() {
-              infowindow.open(map, marker);
-            });
-          } else {
-            console.log("No results found");
-          }
-        } else {
-          console.log("Geocode was not successful for the following reason: " + status);
-        }
-      });
-    }
-
   }
 }
 
